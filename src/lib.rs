89fbd2bcaeb4c176e0582f0a9b2b09532b4a6d4c
Aviral <soniaviral14@gmail.com>
2024-03-11 11:47:14 +0530

    implementing iterators

--- src/lib.rs
+++ src/lib.rs
@@ -23,13 +23,17 @@ pub struct Config {
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            //args in a iterator
+            Some(arg) => arg,
+            None => return Err("did not get any string"),
+        };
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("did not get a file "),
+        };
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         Ok(Config {
             query,
@@ -39,15 +43,10 @@ impl Config {
     }
 }
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
+    //to get the iterator on all the lines of the content we call contents.lines()
+    //then we filter
+    contents.lines().filter(|line| line.contains(query)).collect()
 
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
 }
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
